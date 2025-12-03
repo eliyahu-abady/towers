6b1aa85b3ff4b619fc8869db5b2846e463ab9846
eliyahu abady <eliyahu.abady@gmail.com>
2025-12-03 23:36:01 +0200

    Working game version

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,88 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function Ring({level}) {
-
-  return(
-    <div className='ring' style={{height: 20+level*4, width: 200+level*60}}>{level}</div>
-  )
-}
-
-function Column() {
-  const [rings, setRings] = useState([1,2,3])
-
-  return(      
-      <button className='column'>
-        {rings.map((ring, index) => (
-          <>
-          <Ring key={index} level={ring} />
-          {console.log("run")}
-          </>
-        ))}
-      </button>
-  )
-}
-
 function App() {
+  const [dataGame, setDataGame] = useState([[1],[],[]])
+  const [lifting, setLifting] = useState(null)
+  const [level, setLevel] = useState(0)
+
+  useEffect(() => {
+    if(upLevel())
+      setLevel(prev => prev+1)
+  }, [dataGame])
+
+  useEffect(() => {
+    if(dataGame[0].length === 0) {
+      setDataGame((prev) => {
+        const newData = [...prev]
+        newData[0] = [level]
+        return newData
+      })} else {
+        setDataGame((prev) => {
+        const newData = [...prev]
+        newData[1] = [level]
+        return newData
+      })}
+  }, [level])
+
+  function Ring({level}) {
+    return(
+      <div className='ring' style={{height: 20+level*4, width: 100+level*20}}>
+        <p className='number'>{level}</p>
+      </div>
+    )
+  }
+
+  function Column({array, index, color}) {
+    return(
+        <button className='column' onClick={() => {
+          if(lifting) {
+            if(lifting < array[0] || array.length === 0) {
+              setDataGame((prev) => {
+                const newData = [...prev]
+                newData[index] = [lifting, ...prev[index]]
+                return newData
+              })
+              setLifting(null)
+            }
+          } else {
+          const ring = dataGame[index][0]
+          const newData = [...dataGame]
+          newData[index] = dataGame[index].slice(1)
+          setLifting(ring)
+          setDataGame(newData)
+        }}}>
+          <div className='stick' style={{backgroundColor: color}}></div>
+          {array.map((ring, index) => (
+            <Ring key={index} level={ring} />
+          ))}
+        </button>
+    )
+  }
 
   return (
     <>
       <h1>game</h1>
+      <div className='lifting'>{lifting && <Ring level={lifting}/>} </div>
       <div className='game'>
-        <Column/>
-        <Column/>
-        <Column/>
+        {
+          Array(3).fill(null).map((_, index) => (
+            <Column key={index} array={dataGame[index]} index={index} color={"red"}/>
+          ))
+        }
       </div>
     </>
   )
+
+
+  function upLevel() {
+    return (
+    (dataGame[0].length === 0 && dataGame[1].length === 0 ||
+      dataGame[0].length === 0 && dataGame[2].length === 0 ||
+      dataGame[1].length === 0 && dataGame[2].length === 0) &&
+      lifting === null
+    )
+  }
 }
 
 export default App
