8dbc4267374f8a4558d4452dfc0b3592c1575959
eliyahu abady <eliyahu.abady@gmail.com>
2025-12-31 21:38:39 +0200

    list in menu

--- src/componants/menu.jsx
+++ src/componants/menu.jsx
@@ -2,12 +2,12 @@ function Menu({setScreen}) {
     return(
         <>
             <h1 className="title">towers of hanoi</h1>
-            <div className="menu">
-                <button onClick={() => setScreen("game")}>game</button>
-                <button onClick={() => setScreen("about")}>about</button>
-                <button onClick={() => setScreen("records")}>records</button>
-                <button onClick={() => setScreen("auth")}>sign</button>
-            </div>
+            <ul className="menu">
+                <li><button onClick={() => setScreen("game")}>game</button></li>
+                <li><button onClick={() => setScreen("about")}>about</button></li>
+                <li><button onClick={() => setScreen("records")}>records</button></li>
+                <li><button onClick={() => setScreen("auth")}>sign</button></li>
+            </ul>
         </>
     )
 }
